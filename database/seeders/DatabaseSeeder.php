7407fb2474d1c7231f50ca46cb8c6a3ca4bb1416
AbdelrahmanRamadan92 <abdelrahmanfaisal92@gmail.com>
2023-08-30 00:20:25 +0300

    Make Seeding to admin data and block registeration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // seeding admin Data
+        DB::table('users')->delete();
+        DB::table('users')->insert([
+            'name' =>'Abdelrahman Ramadan',
+            'email' =>'abdelrahman@admin.com',
+            'password' => Hash::make('123456789'),
+        ]);    
     }
 }
---
+++ resources/views/dashboard/admin/auth/login.blade.php
@@ -0,0 +1,63 @@
+@extends('dashboard.layouts.master2')
+@section('css')
+<!-- Sidemenu-respoansive-tabs css -->
+<link href="{{URL::asset('dashboard/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}" rel="stylesheet">
+@endsection
+@section('content')
+		<div class="container-fluid">
+			<div class="row no-gutter">
+				<!-- The image half -->
+				<div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+					<div class="row wd-100p mx-auto text-center">
+						<div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+							<img src="{{URL::asset('dashboard/img/media/login.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
+						</div>
+					</div>
+				</div>
+				<!-- The content half -->
+				<div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+					<div class="login d-flex align-items-center py-2">
+						<!-- Demo content-->
+						<div class="container p-0">
+							<div class="row">
+								<div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
+									<div class="card-sigin">
+										<div class="mb-5 d-flex"> <a href="{{ url('/') }}"><img src="{{URL::asset('dashboard/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Va<span>le</span>x</h1></div>
+										<div class="card-sigin">
+											<div class="main-signup-header">
+												<h2>Welcome back!</h2>
+												<h5 class="font-weight-semibold mb-4">Please sign in to continue.</h5>
+												@if ($errors->any())
+												<div class="alert alert-danger">
+													<ul>
+														@foreach ($errors->all() as $error)
+															<li>{{ $error }}</li>
+														@endforeach
+													</ul>
+												</div>
+												@endif
+												<form method="POST" action="{{ route('login') }}">
+													@csrf
+
+													<div class="form-group">
+														<label>Email</label> <input name="email" id="email" class="form-control" placeholder="Enter your email" type="email" value="{{old('email')}}" required autofocus>
+													</div>
+													<div class="form-group">
+														<label>Password</label> 
+
+														<input type="password" name="password" required class="form-control" placeholder="Enter your password" type="password">
+													</div><button class="btn form-group btn-main-primary btn-block">Log In</button>
+												</form>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div><!-- End -->
+					</div>
+				</div><!-- End -->
+			</div>
+		</div>
+@endsection
+@section('js')
+@endsection
